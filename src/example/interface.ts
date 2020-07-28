4af31c3c1c6c1526a37d39b97c55e07697f641c8
Asam <xiangmaoxian@qq.com>
2020-07-28 23:28:50 +0800

    feat: 看完ts接口这一节，还有些东西没看懂

--- src/example/interface.ts
+++ src/example/interface.ts
@@ -15,7 +15,7 @@
 //   return p.firstName + p.c
 // }
 
-// console.log(test({firstName: 'xx', lastNameObj: {name: '111'}, c: 222})) 
+// console.log(test({firstName: 'xx', lastNameObj: {name: '111'}, c: 222}))
 
 // 接口
 
@@ -30,7 +30,7 @@
 // }
 
 // const myObj = { size: 10, label: 'Size 10 Object', c: '2' }
-// console.log(printLabel(myObj)) 
+// console.log(printLabel(myObj))
 
 // 可选接口
 
@@ -62,9 +62,81 @@
 
 // 定义 函数接口
 
-interface AddFunc { // 调用签名
+interface AddFunc {
+  // 调用签名
   (num1: number, num2: number): number
 }
 
-const addfunc: AddFunc = (n1, n2) => n1+n2
-console.log(addfunc(2,43))
+const addfunc: AddFunc = (n1, n2) => n1 + n2
+console.log(addfunc(2, 43))
+
+// 索引签名
+interface indexSigned {
+  [index: number]: string
+}
+const arr: indexSigned = { 1: '1' }
+console.log(arr)
+
+// 类的接口实现
+interface ClockInterface {
+  currentTime: string
+  setTime(d: string): void
+}
+
+class Clock implements ClockInterface {
+  currentTime: string = '11'
+  setTime(d: string) {
+    this.currentTime = d
+  }
+  // constructor(d: number, h: number) {
+  //   this.currentTime = `${d}: ${h}`
+  // }
+}
+
+// const c = new Clock()
+// console.log(c)
+
+// 混合类型
+interface Counter {
+  (start: number): string
+  interval: number
+  reset(): void
+}
+
+function getCounter(): Counter {
+  const counter = <Counter>function (start: number) {
+    console.log(start)
+  }
+  counter.interval = 123
+  counter.reset = function () {
+    console.log('调用reset')
+  }
+  return counter
+}
+
+const c = getCounter()
+c(10)
+c.reset()
+c.interval = 5.0
+console.dir(c)
+
+// 接口继承类
+class Control {
+  private state: any
+}
+
+interface SelectableControl extends Control {
+  select(): void
+}
+
+class ddd extends Control implements SelectableControl {
+  constructor() {
+    super()
+    // this.state = 222
+  }
+  select(): void {
+    console.log('fff')
+  }
+}
+
+console.log(new ddd())
